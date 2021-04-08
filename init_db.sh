711c1a4a8bf80ef78dccca862d595f41efd96662
badconfig <array.clean@gmail.com>
2021-04-08 22:27:14 +0300

    sdaf

--- init_db.sh
+++ init_db.sh
@@ -1,6 +1,8 @@
 ethblock=12200988
 ftmblock=3517943
 bscblock=6394152
+docker exec -it postgres_db_transactions psql -U docker diesel_db -c "CREATE TABLE extracted_data(id SERIAL PRIMARY KEY,base64Bytes VARCHAR NOT NULL);"
+docker exec -it postgres_db_transactions psql -U docker diesel_db -c "CREATE TABLE poller_states(id SERIAL PRIMARY KEY,num INT NOT NULL DEFAULT 0);"
 docker exec -it postgres_db_transactions psql -U docker diesel_db -c "DELETE * FROM poller_states;"
 docker exec -it postgres_db_transactions psql -U docker diesel_db -c "INSERT INTO poller_states(id,num) VALUES (1,${ethblock}),(2,${ethblock}),(3,${ethblock}),(4,${ethblock}),(5,${ethblock}),(6,${ethblock}),(7,${ethblock}),(8,${ethblock}),(9,${ethblock}),(10,${ethblock}),(11,${ethblock}),(12,${ethblock});"
 docker exec -it postgres_db_transactions psql -U docker diesel_db -c "INTO poller_states(id,num) VALUES (14,${ftmblock}),(15,${ftmblock}),(16,${ftmblock});"
